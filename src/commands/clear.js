bec61d47375fa174547a1dcf23d4d6daeb96b733
Mini <80361877+Mini51@users.noreply.github.com>
2022-08-08 12:45:32 -0400

    added a clear command

---
+++ src/commands/clear.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder, PermissionsBitField } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('clear')
+        .setDescription('clear a set number of messages')
+        .addIntegerOption( option => 
+            option.setName('number')
+            .setDescription('number of messages to clear')
+            .setMaxValue(100)
+            .setMinValue(1)
+            .setRequired(true)
+        ),
+
+
+    async execute(interaction) { 
+        const admin = interaction.guild.members.cache.get(interaction.user.id);
+        const amount = interaction.options.getInteger('number');
+        const channel = interaction.channel;
+        if(!admin.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+            return interaction.reply({ content: 'You do not have permission to clear messages!', ephemeral: true });
+        }
+
+        await channel.bulkDelete(amount);
+        interaction.reply({ content: `Cleared ${amount} messages!`, ephemeral: true });
+    } 
+}
