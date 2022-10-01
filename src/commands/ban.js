9f62bc1c21ac4de58af025c97db5af278876ecf2
200 <rasinrohit@gmail.com>
2022-10-01 22:16:59 +0530

    removed redundant code, fixed bot check 1. Message length is checked by discord 2. Similarly checking if user is in guild 3. Wrong condition to check if target is bot

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -1,54 +1,48 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { EmbedBuilder, PermissionsBitField} = require('discord.js');
+const { EmbedBuilder, PermissionsBitField, CommandInteraction } = require('discord.js');
 
 
 module.exports = {
-    helpinfo : "This comand is used to ban a user from the server",
+    helpinfo: "This comand is used to ban a user from the server",
     data: new SlashCommandBuilder()
         .setName('ban')
         .setDescription('ban a user')
-        .addUserOption(option => 
+        .addUserOption(option =>
             option.setName('user')
-            .setDescription('user to ban')
-            .setRequired(true)
+                .setDescription('user to ban')
+                .setRequired(true)
         )
         .addStringOption(option => option
             .setName('reason')
             .setDescription('reason for ban')
             .setMaxLength(400)
-        ), 
-
-    async execute(interaction) { 
+        ),
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     * @returns 
+     */
+    async execute(interaction) {
         const member = interaction.guild.members.cache.get(interaction.options.getUser('user').id);
-        const reason = interaction.options.getString('reason');
+        const reason = interaction.options.getString('reason') ?? "";
         const moderator = interaction.guild.members.cache.get(interaction.user.id);
-
-
-        if(reason.length > 400) {
-            return interaction.reply({ content: 'Reason is too long!', ephemeral: true });
-        }
-
-        // makes sure the user is in the guild
-        if(!member) {
-            return interaction.reply({ content: 'User is not in the guild', ephemeral: true });
-        }
 
         // makes sure the user has the ban permission
         if (!moderator.permissions.has(PermissionsBitField.Flags.BanMembers)) {
             return interaction.reply({ content: 'You do not have permission to ban members!', ephemeral: true });
         }
 
-        // makes sure the user is not the bot
-        if(member.bot) {
+        // makes sure the user is not the bot        
+        if (member.user.bot) {
             return interaction.reply({ content: 'You cannot ban a bot!', ephemeral: true });
         }
 
         // makes sure the user is not admin
-        if(member === moderator) {
+        if (member === moderator) {
             return interaction.reply({ content: 'You cannot ban yourself!', ephemeral: true });
         }
-        
-        if(!member.bannable) {
+
+        if (!member.bannable) {
             return interaction.reply({ content: `I cannot ban this user`, ephemeral: true });
         }
 
@@ -56,7 +50,7 @@ module.exports = {
 
 
 
-            
+
         member.ban({ reason: `${moderator} - ${reason || 'No reason provided'}` });
 
 
